3d8bf32db52fb902d71a68a4bbd620e61618909a
Jordan Green <jagman1x3@gmail.com>
2015-04-07 16:04:54 -0400

    Skip single line comments and clean up code

--- C++ Tokenizer/Tokenizer.cpp
+++ C++ Tokenizer/Tokenizer.cpp
@@ -11,7 +11,7 @@ using namespace std;
 const set<char> SINGLE_SYMBOLS = { '!', '%', '&', '(', ')', '*', '+', ',', '-', '-', '/', ';', '<', '=', '>', '{', '|', '}', '~', 
 									'.', ',', ':', '\'', '"', '?', '[', ']', '#'};
 const set<string> DOUBLE_SYMBOLS = { "!=", "&&", "<<", "<=", "==", ">=", ">>", "||", "::", "+=", "-=", "*=", "/", "%=", "&=", "^=",
-									"<<=", ">>=", "->", "->*", ".*"};
+									"<<=", ">>=", "->", "->*", ".*", "//"};
 const set<string> KEYWORDS = { "alignas", "alignof", "and", "and_eq", "asm", "auto", "bitand", "bitor", "bool", "break", "case", 
 	"catch","char", "char16_t", "char32_t", "class", "compl", "concept", "const", "constexpr", "const_cast", "continue", "decltype",
 	"default", "delete", "do", "double", "dynamic_cast", "else", "enum", "explicit", "export", "extern", "false", "float", "for", 
@@ -22,7 +22,7 @@ const set<string> KEYWORDS = { "alignas", "alignof", "and", "and_eq", "asm", "au
 int UNKNOWN = 0, IDENT = 298, INTEGER = 299;
 const int MAX_SYMBOL_SIZE = 3;
 map<string, int> tokenMap;
-vector<Token> tokens;
+
 
 void makeTokenMap(){
 	int code = 300;
@@ -55,59 +55,47 @@ int getType(const string s){
 	return it->second;
 }
 
-int main(int argc, char** argv){
-	
-	cout << "Enter a filename." << endl;
-	string filename;
-	cin >> filename;
-	cin.ignore(100, '\n');
-
-	ifstream is(filename);
-	if (!is){
-		cout << "Error: Couldn't open the file." << endl;
-		cin.ignore();
-		return 2;
-	}
-
+void tokenize(ifstream* is, vector<Token>* tokens){
 	makeTokenMap();
 
 	char c;
-	while (is >> c){
+	// using is.get() here makes it loop on whitespace at end of file
+	while (*is >> c){
 		string str;
 		int tokenType = UNKNOWN;
 		if (isalpha(c)){
 			str += c;
-			is.get(c);
+			is->get(c);
 			while (isalnum(c) || c == '_'){
 				str += c;
-				is.get(c);
+				is->get(c);
 			}
-			is.unget();
+			is->unget();
 		}
 		else if (isdigit(c)){
 			str += c;
-			is.get(c);
+			is->get(c);
 			while (isdigit(c)){
 				str += c;
-				is.get(c);
+				is->get(c);
 			}
-			is.unget();
+			is->unget();
 			tokenType = INTEGER;
 		}
 		else if (isSingleSymbol(c)){
 			str += c;
 			for (int j = 0; j < MAX_SYMBOL_SIZE - 1; j++){
-				is.get(c);
+				is->get(c);
 				if (isSingleSymbol(c)){
 					str += c;
 				}
 				else{
-					is.unget();
+					is->unget();
 					break;
 				}
 				if (!isDoubleSymbol(str)){
 					str = str.substr(0, str.size() - 1);
-					is.unget();
+					is->unget();
 					break;
 				}
 			}
@@ -116,15 +104,38 @@ int main(int argc, char** argv){
 		if (str.empty()){
 			continue;
 		}
+		//ignore comments
+		if (str.compare("//") == 0){
+			is->get(c);
+			while (c != '\n'){
+				is->get(c);
+			}
+			continue;
+		}
 
 		if (tokenType != INTEGER){
 			tokenType = getType(str);
 		}
 		Token* token = new Token(str, tokenType);
-		tokens.push_back(*token);
+		tokens->push_back(*token);
 
 	}
-	
+}
+
+int main(int argc, char** argv){
+	cout << "Enter a filename." << endl;
+	string filename;
+	cin >> filename;
+	cin.ignore(100, '\n');
+	ifstream is(filename);
+	if (!is){
+		cout << "Error: Couldn't open the file." << endl;
+		cin.ignore();
+		return 2;
+	}
+
+	vector<Token> tokens;
+	tokenize(&is, &tokens);
 	for (int i = 0; i < tokens.size(); i++){
 		cout << tokens[i].value << "\t\t" << tokens[i].type << endl;
 	}
